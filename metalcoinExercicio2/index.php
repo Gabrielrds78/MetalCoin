7fbe380d10a4978f4787447a35222eb0570057c4
gabriel.rds <100387568+Gabrielrds78@users.noreply.github.com>
2023-04-05 21:46:26 -0300

    Add files via upload

---
+++ metalcoinExercicio2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="icon" href="/img/logo.png">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body class="container">
+    <nav class="retangulo ">
+        <img class="logo nav_bar1" src="img/logo.png" alt="">
+        <a class="text text_style" href="#">HOME</a>
+        <a class="text text_style" href="produtos.php">PRODUTOS</a>
+        <a class="text text_style" href="">SERVIÇOS</a>
+        <a href="cadastre.php" class="conta text_style">Conta</a>
+        <img src="img/usuario-de-perfil.png" class="perfil" alt="">
+    </nav>
+    <footer class="rodape ">
+        <label>E-MAIL: VIXMIN@GMAIL.COM</label>
+        <label for="">|</label>
+        <label>TELEFONE: (27) 12345-6789</label>
+        
+</footer>
+</body>
+</html>
